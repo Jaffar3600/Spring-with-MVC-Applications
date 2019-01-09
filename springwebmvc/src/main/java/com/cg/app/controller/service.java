db6b5f13a5b292aaaedb5891633992a60b0c7c7e
SHAIK <jaffar.shaik@capgemini.com>
2019-01-09 18:41:05 +0530

    Adds Spring MVC Applications

---
+++ springwebmvc/src/main/java/com/cg/app/controller/service.java
@@ -0,0 +1,12 @@
+package com.cg.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class service {
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+}
